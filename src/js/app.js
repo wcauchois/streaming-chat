e46600380f17335d4640ac281ddf2cf92e031cb3
Bill Cauchois <wcauchois@foursquare.com>
2014-12-24 21:57:24 +0000

    websocket chat working modulo bug

--- src/js/app.js
+++ src/js/app.js
@@ -23,6 +23,9 @@ var rpcStream = mx.createStream('rpc');
 rpcStream.pipe(d).pipe(rpcStream);
 
 window.chatModel = chatModel;
+chatModel.on('item', function(item) {
+  console.log(item);
+});
 
 var HelloMessage = React.createClass({
   render: function() {
@@ -30,4 +33,95 @@ var HelloMessage = React.createClass({
   }
 });
 
-React.render(<HelloMessage name="Bill" />, document.getElementById('container'));
+var ChatList = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <div className="row" style={{"margin-top": "10%"}}>
+          <ul className="message-list">
+            {this.props.lines.map(function(line) {
+              return (
+                <li key={line.__id}>
+                  <span className="nick">{line.nick || 'unknown'}:</span>
+                  {"\u00a0"}
+                  <span className="message">{line.message || ''}</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <InputSection sendMessage={this.props.sendMessage} />
+      </div>
+    );
+  }
+});
+
+var InputSection = React.createClass({
+  getInitialState: function() {
+    return {
+      value: '',
+      nickValue: window.localStorage.getItem('nickname') || ''
+    };
+  },
+
+  handleNickChange: function(event) {
+    this.setState({nickValue: event.target.value});
+    window.localStorage.setItem('nickname', event.target.value);
+  },
+
+  handleChange: function(event) {
+    this.setState({value: event.target.value});
+  },
+
+  doSend: function() {
+    this.props.sendMessage({
+      message: this.state.value,
+      nick: this.state.nickValue
+    });
+    this.setState({value: ''});
+  },
+
+  handleKeyDown: function(event) {
+    if (event.key === 'Enter') {
+      this.doSend();
+    }
+  },
+
+  render: function() {
+    return (
+      <div className="row">
+        <div className="two columns">
+          <input className="u-full-width" type="text" value={this.state.nickValue}
+            onChange={this.handleNickChange} placeholder="Nickname" />
+        </div>
+        <div className="eight columns">
+          <input className="u-full-width" type="text" value={this.state.value}
+            onChange={this.handleChange} onKeyPress={this.handleKeyDown} />
+        </div>
+        <div className="two columns">
+          <button className="button-primary" onClick={this.doSend}>Send</button>
+        </div>
+      </div>
+    );
+  }
+});
+
+function sendMessage(message) {
+  chatModel.push(message);
+}
+window.sendMessage = sendMessage;
+
+var allMessages = [];
+function renderMessages() {
+  React.render(
+    <ChatList lines={allMessages} sendMessage={sendMessage} />,
+    document.getElementById('container')
+  );
+}
+renderMessages();
+chatModel.on('item', function(message) {
+  allMessages.push(message);
+  renderMessages();
+});
+
+
