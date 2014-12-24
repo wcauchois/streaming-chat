a9a3bcd305e9f26ca16c18a783af57961030d799
Bill Cauchois <wcauchois@foursquare.com>
2014-12-24 21:06:03 +0000

    multiplex RPC stream

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var express = require('express'),
     config = require('config'),
     path = require('path'),
     jade = require('jade'),
-    util = require('util');
+    util = require('util'),
+    WebSocketServer = require('ws').Server,
+    websocket = require('websocket-stream'),
+    _ = require('underscore'),
+    AppendOnly = require('append-only'),
+    http = require('http'),
+    MuxDemux = require('mux-demux'),
+    dnode = require('dnode');
 
 var app = express();
 
@@ -18,7 +25,40 @@ app.get('/', function(req, res) {
   res.render('index.jade');
 });
 
-app.listen(config.port, function() {
+var server = http.createServer(app);
+server.listen(config.port, function() {
   util.log('Listening on port ' + config.port);
+});
+
+var chatModel = AppendOnly();
+chatModel.on('item', function(item) {
+  util.log('Got chat item: ' + JSON.stringify(item));
+});
+
+var rpcs = {
+  testRpc: function() {
+    util.log("Got a call to testRpc");
+  }
+};
+
+var wss = new WebSocketServer({server: server});
+wss.on('connection', function(ws) {
+  util.log('Connected WebSocket');
+  var websocketStream = websocket(ws);
+  var modelStream, rpcStream, mx;
+
+  websocketStream.pipe(mx = MuxDemux(function(stream) {
+    if (stream.meta === 'model') {
+      modelStream = chatModel.createStream();
+      stream.pipe(modelStream).pipe(stream);
+    } else if (stream.meta === 'rpc') {
+      rpcStream = dnode(rpcs);
+      stream.pipe(rpcStream).pipe(stream);
+    }
+  })).pipe(websocketStream);
+
+  ws.on('close', function() {
+    util.log('Disconnected WebSocket');
+  });
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
-var uniq = require('uniq');
-var nums = [1, 2, 3, 5, 3, 4, 2, 1, 5, 3];
-console.log('hello from browserify: ' + uniq(nums).join(', '));
+var AppendOnly = require('append-only'),
+    websocket = require('websocket-stream'),
+    MuxDemux = require('mux-demux'),
+    dnode = require('dnode');
+
+var chatModel = AppendOnly();
+var websocketPath = 'ws' + (location.protocol === 'https:' ? 's' : '') + '://' +
+  location.hostname + (location.port ? (':' + location.port) : '');
+var ws = websocket(websocketPath);
+
+var mx = MuxDemux();
+ws.pipe(mx).pipe(ws);
+
+var modelStream = mx.createStream('model');
+modelStream.pipe(chatModel.createStream()).pipe(modelStream);
+
+var d = dnode();
+d.on('remote', function(remote) {
+  window.remote = remote;
+});
+
+var rpcStream = mx.createStream('rpc');
+rpcStream.pipe(d).pipe(rpcStream);
+
+window.chatModel = chatModel;
